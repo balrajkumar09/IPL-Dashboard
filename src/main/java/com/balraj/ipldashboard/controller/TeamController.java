7700744addaccfbc8a006c51cd49ac7f2f373646
Balraj Kumar <balrajkumar09@gmail.com>
2021-11-09 15:14:29 +0530

    React App with Team Page

--- src/main/java/com/balraj/ipldashboard/controller/TeamController.java
+++ src/main/java/com/balraj/ipldashboard/controller/TeamController.java
@@ -4,12 +4,13 @@ import com.balraj.ipldashboard.model.Team;
 import com.balraj.ipldashboard.repository.MatchRepository;
 import com.balraj.ipldashboard.repository.TeamRepository;
 
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
